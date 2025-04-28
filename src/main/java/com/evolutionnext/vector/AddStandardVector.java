dd7cb230f2b78e35824aae97b9f6e53297013732
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2025-04-28 15:21:03 -0600

    Formatted code

--- src/main/java/com/evolutionnext/vector/AddStandardVector.java
+++ src/main/java/com/evolutionnext/vector/AddStandardVector.java
@@ -18,8 +18,10 @@ public class AddStandardVector {
 
     public static void main(String[] args) {
 
-        float[] a = {34.2f, 89.8f, 45.9f, 78.3f, 67.5f, 12.6f, 23.9f, 101.8f, 90.7f, 85.6f};
-        float[] b = {24.5f, 32.6f, 38.9f, 28.2f, 14.3f, 67.5f, 54.5f, 77.8f, 46.5f, 89.5f};
+        float[] a = {34.2f, 89.8f, 45.9f, 78.3f, 67.5f,
+                    12.6f, 23.9f, 101.8f, 90.7f, 85.6f};
+        float[] b = {24.5f, 32.6f, 38.9f, 28.2f, 14.3f,
+                     67.5f, 54.5f, 77.8f, 46.5f, 89.5f};
         float[] c = new float[a.length];
 
         System.out.printf("a length %d%n", a.length);
--- src/main/java/com/evolutionnext/vector/CompareVector.java
+++ src/main/java/com/evolutionnext/vector/CompareVector.java
@@ -37,7 +37,8 @@ public class CompareVector {
 
     public static void main(String[] args) {
 
-        float[] a = {0.23f, 0.45f, 0.49f, 0.90f, 0.01f, 0.88f, 0.79f, 0.45f, 0.21f, 0.93f};
+        float[] a = {0.23f, 0.45f, 0.49f, 0.90f, 0.01f,
+            0.88f, 0.79f, 0.45f, 0.21f, 0.93f};
         System.out.printf("a length %d%n", a.length);
 
         vectorComputation(a);
